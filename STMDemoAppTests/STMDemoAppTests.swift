19ec0afca3a029654fdefe327d0993ab1713030a
Rajesh Ramachandrakurup <rajesh.ramachandrakurup@foxsports.com.au>
2020-10-28 21:14:44 +1100

    Changes to config and tests

--- STMDemoAppTests/STMDemoAppTests.swift
+++ STMDemoAppTests/STMDemoAppTests.swift
@@ -21,6 +21,8 @@ class STMDemoAppTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let a = 10
+        XCTAssertEqual(a, 10)
     }
 
     func testPerformanceExample() throws {
